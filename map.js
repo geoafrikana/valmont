5bf43d04978caf64bf329096218bdfd307f717cd
geoafrikana <geoafrikana@gmail.com>
2023-02-25 09:10:58 +0100

    loaded wfs

--- map.js
+++ map.js
@@ -4,21 +4,24 @@ var OpenStreetMap  = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-const WmsUrl = 'https://val.aponiawebsolutions.ca/geoserver/valmont/wms'
+const WmsUrl = 'https://val.aponiawebsolutions.ca/geoserver/wms'
+const WfsUrl = 'https://val.aponiawebsolutions.ca/geoserver/wfs'
 
 var topographic = L.tileLayer.wms(WmsUrl, {
     layers: 'color_3857',
     format: 'image/png',
     styles: 'color_style',
     opacity: 0.5,
+    tiled: true,
     attribution: "Aponia Web Solutions"
-}).addTo(map);
+}) //.addTo(map);
 
 var DEMBlackAndWhite = L.tileLayer.wms(WmsUrl, {
     layers: 'dem_3857',
     format: 'image/png',
     styles: 'raster',
     opacity: 0.5,
+    tiled: true,
     attribution: "Aponia Web Solutions"
 })
 
@@ -27,8 +30,11 @@ var DEMColor = L.tileLayer.wms(WmsUrl, {
     format: 'image/png',
     styles: 'dem_topo_style',
     opacity: 0.5,
+    tiled: true,
     attribution: "Aponia Web Solutions"
 })
+
+
 
 var baseLayers = {
     OpenStreetMap
@@ -40,12 +46,51 @@ var overlayLayers = {
     'Topographic': topographic
 }
 
+
+
 L.control.layers(baseLayers, overlayLayers).addTo(map);
 
 
 L.control
     .opacity(overlayLayers, {
         label: 'Layers Opacity',
-    })
-    .addTo(map);
+    }).addTo(map);
 
+
+    var getBBox = ()=>{
+        var bounds = map.getBounds();
+        bounds = [bounds._southWest.lng, bounds._southWest.lat, bounds._northEast.lng, bounds._northEast.lat].join(',')
+        return bounds
+    }
+
+    // var bounds = 
+    $.ajax(WfsUrl,{
+        type: 'GET',
+        data: {
+          service: 'WFS',
+          version: '1.1.0',
+          request: 'GetFeature',
+          typename: 'valmont:dummy_places',
+          srsname: 'EPSG:4326',
+          outputFormat: 'text/javascript',
+        //   bbox:  bounds
+          },
+        dataType: 'jsonp',
+        jsonpCallback:'callback:handleJson',
+        jsonp:'format_options'
+       });
+        //Geojson style file
+        var myStyle = {
+          'color': 'red'
+        }
+      // the ajax callback function
+      function handleJson(data) {
+        console.log(data.features.length)
+          selectedArea = L.geoJson(data, {
+            style: myStyle,
+            onEachFeature: function(feature, layer) {
+              layer.bindPopup(`ID: ${feature.properties.fid}`)
+            }
+          }).addTo(map);
+        map.fitBounds(selectedArea.getBounds());
+      }
